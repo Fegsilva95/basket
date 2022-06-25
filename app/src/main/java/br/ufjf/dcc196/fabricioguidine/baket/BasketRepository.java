d922cc4a39a6f04939e596beb84e1cb93234e0f0
Fgsilva <fgsilva@ice.ufjf.br>
2022-06-25 20:33:24 -0300

    Add distance, name and reset button to basket app - activity 5

--- app/src/main/java/br/ufjf/dcc196/fabricioguidine/baket/BasketRepository.java
+++ app/src/main/java/br/ufjf/dcc196/fabricioguidine/baket/BasketRepository.java
@@ -8,6 +8,8 @@ public class BasketRepository {
     private SharedPreferences preferences;
     private final String PREFERENCES_NAME = "br.ufjf.basket";
     public final static String POINTS_KEY = "POINTS_KEY";
+    public final static String THROWS_KEY = "THROWS_KEY";
+    public final static String BLOCKS_KEY = "BLOCKS_KEY";
 
     public BasketRepository(Context context) {
         this.context = context;
@@ -33,5 +35,46 @@ public class BasketRepository {
         Integer points = getPoints() - 1;
         setPoints(points);
     }
+
+    public Integer getThrows() {
+        return preferences.getInt(THROWS_KEY, 0);
+    }
+
+    public void setThrows(Integer throwsValue) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(THROWS_KEY, throwsValue);
+        editor.apply();
+    }
+
+    public void incThrows() {
+        Integer throwsValue = getThrows() + 1;
+        setThrows(throwsValue);
+    }
+
+    public void decThrows() {
+        Integer throwsValue = getThrows() - 1;
+        setThrows(throwsValue);
+    }
+
+    public Integer getBlocks() {
+        return preferences.getInt(BLOCKS_KEY, 0);
+    }
+
+    public void setBlocks(Integer blocksValue) {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(BLOCKS_KEY, blocksValue);
+        editor.apply();
+    }
+
+    public void incBlocks() {
+        Integer blocksValue = getBlocks() + 1;
+        setBlocks(blocksValue);
+    }
+
+    public void decBlocks() {
+        Integer blocksValue = getBlocks() - 1;
+        setBlocks(blocksValue);
+    }
+
 
 }
--- app/src/main/java/br/ufjf/dcc196/fabricioguidine/baket/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/fabricioguidine/baket/MainActivity.java
@@ -6,13 +6,26 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
     TextView pointsValue;
     Button pointsPlusButton;
     Button pointsMinusButton;
+    TextView throwsValue;
+    Button throwPlusButton;
+    Button throwMinusButton;
+    TextView blocksValue;
+    Button blocksPlusButton;
+    Button blocksMinusButton;
     BasketRepository repo;
+    String playerName;
+    EditText namePlainText;
+    Double distance;
+    EditText distancePlainText;
+    TextView message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +36,18 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         pointsPlusButton = findViewById(R.id.pointsPlusButton);
         pointsMinusButton = findViewById(R.id.pointsMinusButton);
 
+        throwsValue = findViewById(R.id.throwsValue);
+        throwPlusButton = findViewById(R.id.throwPlusButton);
+        throwMinusButton = findViewById(R.id.throwMinusButton);
+
+        blocksValue = findViewById(R.id.blockValue);
+        blocksPlusButton = findViewById(R.id.blockPlusButton);
+        blocksMinusButton = findViewById(R.id.blockMinusButton);
+
         repo = new BasketRepository(getApplicationContext());
         pointsValue.setText(repo.getPoints().toString());
+        throwsValue.setText(repo.getThrows().toString());
+        blocksValue.setText(repo.getBlocks().toString());
     }
 
     public void clickIncDec(View v){
@@ -37,15 +60,68 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                 repo.decPoints();
                 pointsValue.setText(repo.getPoints().toString());
                 break;
+            case R.id.throwPlusButton:
+                repo.incThrows();
+                throwsValue.setText(repo.getThrows().toString());
+                break;
+            case R.id.throwMinusButton:
+                repo.decThrows();
+                throwsValue.setText(repo.getThrows().toString());
+                break;
+            case R.id.blockPlusButton:
+                repo.incBlocks();
+                blocksValue.setText(repo.getBlocks().toString());
+                break;
+            case R.id.blockMinusButton:
+                repo.decBlocks();
+                blocksValue.setText(repo.getBlocks().toString());
+                break;
         }
     }
 
+    public void saveName(View v){
+        namePlainText = findViewById(R.id.namePlainText);
+        playerName = namePlainText.getText().toString();
+
+        message = findViewById(R.id.playerTextView);
+        message.setText("Player: " + playerName);
+    }
+
+    public void saveDistance(View v){
+        distancePlainText = findViewById(R.id.distancePlainText);
+        distance = Double.parseDouble(distancePlainText.getText().toString());
+
+        message = findViewById(R.id.playerTextView);
+        message.setText("Player: " + playerName + ", Distance: " + distance.toString());
+    }
+
+    public void resetValues(View v){
+        repo.setBlocks(0);
+        repo.setThrows(0);
+        repo.setPoints(0);
+        namePlainText = findViewById(R.id.namePlainText);
+        namePlainText.setText("");
+        distancePlainText.setText("0.0");
+
+        playerName = "";
+        distance = 0.0;
+        message.setText("");
+        blocksValue.setText(repo.getBlocks().toString());
+        throwsValue.setText(repo.getThrows().toString());
+        pointsValue.setText(repo.getPoints().toString());
+    }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
     switch (key){
         case BasketRepository.POINTS_KEY:
             pointsValue.setText(repo.getPoints().toString());
+            break;
+        case BasketRepository.THROWS_KEY:
+            throwsValue.setText(repo.getThrows().toString());
+            break;
+        case BasketRepository.BLOCKS_KEY:
+            blocksValue.setText(repo.getBlocks().toString());
             break;
     }}
 }
